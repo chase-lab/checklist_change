0ee50d18719d3c8273b309c93852d721cead81dc
Alban Sagouis <alban.sagouis@idiv.de>
2022-07-04 16:22:24 +0200

    running download and wrangling in isolated environments

--- R/2.0_wrangling_raw_data.r
+++ R/2.0_wrangling_raw_data.r
@@ -1,4 +1,8 @@
 # Wrangle raw data
 
 dir.create("data/wrangled data", showWarnings = FALSE)
-lapply(X = list.files(path = "./R/data wrangling", full.names = TRUE), base::source, local = TRUE, echo = FALSE)
+
+for (script_path in list.files(path = "./R/data wrangling", full.names = TRUE)) {
+   unique_env <- new.env()
+   base::source(file = script_path, local = unique_env, echo = FALSE)
+}
